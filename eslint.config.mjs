6e80c4c85dc779a61f8f414810627997c667bb3e
Mou <m.mounir.f@gmail.com>
2025-07-15 22:18:56 +0300

    chore: initiate format dev tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,22 @@
 import { defineConfig } from "eslint/config";
 import expoConfig from "eslint-config-expo/flat.js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-	expoConfig,
-	{
-		ignores: [
-			"dist/*",
-			"node_modules/*",
-			".expo/*",
-			"build/*",
-			"web-build/*",
-			"expo-env.d.ts",
-			"*.d.ts",
-			".next/*",
-			"out/*",
-			"coverage/*",
-		],
-	},
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: [
+      "dist/*",
+      "node_modules/*",
+      ".expo/*",
+      "build/*",
+      "web-build/*",
+      "expo-env.d.ts",
+      "*.d.ts",
+      ".next/*",
+      "out/*",
+      "coverage/*",
+    ],
+  },
 ]);
