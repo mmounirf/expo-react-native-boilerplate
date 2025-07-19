7223f9ec4ef794b85def9792ec3ed43c28477ec2
Mou <m.mounir.f@gmail.com>
2025-07-19 17:45:50 +0300

    feat: init supabase client

---
+++ lib/supabase.ts
@@ -0,0 +1,17 @@
+import "react-native-url-polyfill/auto";
+import { createClient, processLock } from "@supabase/supabase-js";
+import { SecureStoreAdapter } from "./SecureStoreAdapter";
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL!,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  {
+    auth: {
+      storage: SecureStoreAdapter,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+      lock: processLock,
+    },
+  }
+);
