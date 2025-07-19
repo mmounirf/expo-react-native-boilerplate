e2ee7aabfb5d64423041a04a09f3172f76b592a4
Mou <m.mounir.f@gmail.com>
2025-07-19 17:45:24 +0300

    feat: add SecureStoreAdapter

---
+++ lib/SecureStoreAdapter.ts
@@ -0,0 +1,15 @@
+import type { SupportedStorage } from "@supabase/supabase-js";
+import * as SecureStore from "expo-secure-store";
+
+export const SecureStoreAdapter: SupportedStorage = {
+  getItem: async (key: string) => {
+    const result = await SecureStore.getItemAsync(key);
+    return result ?? null;
+  },
+  setItem: async (key: string, value: string) => {
+    await SecureStore.setItemAsync(key, value);
+  },
+  removeItem: async (key: string) => {
+    await SecureStore.deleteItemAsync(key);
+  },
+};
