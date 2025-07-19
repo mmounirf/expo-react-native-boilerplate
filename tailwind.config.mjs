59ad645a8dd2fb3062bc33afa80c33e5b376abf3
Mou <m.mounir.f@gmail.com>
2025-07-19 16:41:54 +0300

    feat: init tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+import nativewindPreset from "nativewind/preset";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/**/*.{js,ts,tsx}"],
+  presets: [nativewindPreset],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
