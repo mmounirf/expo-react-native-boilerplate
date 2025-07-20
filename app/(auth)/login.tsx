80a6dab043f4cfc6ca26bf313019e9edd356b78e
Mou <m.mounir.f@gmail.com>
2025-07-20 08:55:00 +0300

    feat: sample login and register screens

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View className="flex-1 items-center justify-center gap-2 bg-white dark:bg-black">
+      <Text className="text-2xl font-bold">Login Screen</Text>
+
+      <Link
+        href="/register"
+        className="p-2 rounded-md bg-blue-500 text-white"
+        replace
+      >
+        Register new account
+      </Link>
+    </View>
+  );
+}
---
+++ app/(auth)/register.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function RegisterScreen() {
+  return (
+    <View className="flex-1 items-center justify-center gap-2 bg-white dark:bg-black">
+      <Text className="text-2xl font-bold">Register Screen</Text>
+
+      <Link
+        href="/login"
+        className="p-2 rounded-md bg-blue-500 text-white"
+        replace
+      >
+        Login instead
+      </Link>
+    </View>
+  );
+}
