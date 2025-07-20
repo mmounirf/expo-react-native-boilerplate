fdbbf6b4c6b7161bcf32768ea8f1124107c7b6d1
Mou <m.mounir.f@gmail.com>
2025-07-20 11:09:35 +0300

    fix(android): explicilty add index screen in RootNavigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import '../global.css'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+
+import '../global.css'
 
 function RootNavigator() {
   return (
     <SafeAreaView className="flex-1 bg-white dark:bg-black">
       <StatusBar translucent animated style="auto" />
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
         <Stack.Screen name="(auth)" />
         <Stack.Screen name="(app)" />
       </Stack>
