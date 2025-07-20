9d0674ae56e48797bf7251bde6e0dcd46b47c663
Mou <m.mounir.f@gmail.com>
2025-07-20 11:11:07 +0300

    feat: consume useSecureStore

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,42 @@
 import { Link } from 'expo-router'
-import { Switch, Text, View } from 'react-native'
+import { Button, Switch, Text, View } from 'react-native'
+import { useSecureStore } from '~/hooks/useSecureStore'
 import { useColorScheme } from '~/lib/useColorScheme'
+
+type StoreType = {
+  id: string
+  payload: string
+}
 
 const HomeScreen = () => {
   const { setColorScheme, colorScheme } = useColorScheme()
 
+  const { state, setState } = useSecureStore<StoreType>('testSecureStore')
+
   return (
     <View className="flex-1 items-center justify-center gap-2 bg-white dark:bg-black">
+      <Text className="text-2xl font-bold">State value is</Text>
+      <Text className="text-md text-slate-600">
+        {state?.payload ?? 'EMPTY'}
+      </Text>
+      <Button
+        title="Update State"
+        onPress={() =>
+          setState({
+            id: 'newId',
+            payload: 'This is securely stored and encryped value',
+          })
+        }
+      />
+      <Button
+        title="Remove State"
+        onPress={() =>
+          setState({
+            id: 'newId',
+            payload: 'This is securely stored and encryped value',
+          })
+        }
+      />
       <View className="flex flex-row gap-2">
         <Link href="/login" className="rounded-md bg-blue-500 p-2 text-white">
           <Text>Go to Sign In</Text>
