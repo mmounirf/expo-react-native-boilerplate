0a40d4c33aee42e0382445a3971cf00e85f32399
Mou <m.mounir.f@gmail.com>
2025-07-20 08:54:09 +0300

    feat: root layout groups stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,24 @@
-import { Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+function RootNavigator() {
+  return (
+    <SafeAreaView className="flex-1 bg-white dark:bg-black">
+      <StatusBar translucent animated style="auto" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(app)" />
+      </Stack>
+    </SafeAreaView>
+  );
+}
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="flex-1 bg-white dark:bg-black">
-        <StatusBar translucent animated style="auto" />
-        <Slot />
-      </SafeAreaView>
+      <RootNavigator />
     </SafeAreaProvider>
   );
 }
