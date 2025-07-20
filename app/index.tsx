82792d05a7d76a53a052ae44ae8282890d47676d
Mou <m.mounir.f@gmail.com>
2025-07-20 08:55:51 +0300

    feat: test file based routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,33 @@
+import { Link } from "expo-router";
 import { Switch, Text, View } from "react-native";
 import { useColorScheme } from "~/lib/useColorScheme";
 
-export default function HomeScreen() {
+const HomeScreen = () => {
   const { setColorScheme, colorScheme } = useColorScheme();
 
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className="text-black dark:text-white">
-        Hello from index.tsx 👋
-      </Text>
-      <Text className="text-black dark:text-white">
-        Current theme: {colorScheme}
-      </Text>
+    <View className="flex-1 items-center justify-center gap-2 bg-white dark:bg-black">
+      <View className="flex flex-row gap-2">
+        <Link href="/login" className="p-2 rounded-md bg-blue-500 text-white">
+          <Text>Go to Sign In</Text>
+        </Link>
+        <Link
+          href="/register"
+          className="p-2 rounded-md bg-blue-500 text-white"
+        >
+          <Text>Go to Sign Up</Text>
+        </Link>
+      </View>
+      <Link href="/home" className="p-2 rounded-md bg-green-500 text-white">
+        <Text>Go Home</Text>
+      </Link>
+
       <Switch
         onValueChange={(isDark) => setColorScheme(isDark ? "dark" : "light")}
         value={colorScheme === "dark"}
       />
     </View>
   );
-}
+};
+
+export default HomeScreen;
