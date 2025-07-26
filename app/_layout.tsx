f0d90113a8d6c81fc6f27943796931b8587b1ac1
Mou <m.mounir.f@gmail.com>
2025-07-26 22:16:08 +0300

    remove SafeAreaProvider as it is used internally in rnr primitives as a hook

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,11 @@ import {
 } from '@react-navigation/native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { NAV_THEME } from '~/lib/constants'
 
 import { AuthProvider, useAuth } from '~/context/AuthContext'
+import '~/global.css'
 import { useColorScheme } from '~/lib/useColorScheme'
-import '../global.css'
 
 const LIGHT_THEME: Theme = {
   ...DefaultTheme,
@@ -28,28 +27,24 @@ function RootNavigator() {
 
   return (
     <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      <SafeAreaView className="flex-1">
-        <StatusBar translucent animated style="auto" />
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Protected guard={!session}>
-            <Stack.Screen name="(auth)" />
-          </Stack.Protected>
-          <Stack.Protected guard={!!session}>
-            <Stack.Screen name="(app)" />
-          </Stack.Protected>
-        </Stack>
-      </SafeAreaView>
+      <StatusBar translucent animated style="auto" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Protected guard={!session}>
+          <Stack.Screen name="(auth)" />
+        </Stack.Protected>
+        <Stack.Protected guard={!!session}>
+          <Stack.Screen name="(app)" />
+        </Stack.Protected>
+      </Stack>
     </ThemeProvider>
   )
 }
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <AuthProvider>
-        <RootNavigator />
-      </AuthProvider>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <RootNavigator />
+    </AuthProvider>
   )
 }
