7ef72271dbf87651ce8055156c790fe6d244376e
Mou <m.mounir.f@gmail.com>
2025-07-26 21:53:06 +0300

    add theme provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,46 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from '@react-navigation/native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { NAV_THEME } from '~/lib/constants'
 
 import { AuthProvider, useAuth } from '~/context/AuthContext'
+import { useColorScheme } from '~/lib/useColorScheme'
 import '../global.css'
 
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+}
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+}
+
 function RootNavigator() {
+  const { isDarkColorScheme } = useColorScheme()
   const { session } = useAuth()
 
   return (
-    <SafeAreaView className="flex-1 bg-white dark:bg-black">
-      <StatusBar translucent animated style="auto" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Protected guard={!session}>
-          <Stack.Screen name="(auth)" />
-        </Stack.Protected>
-        <Stack.Protected guard={!!session}>
-          <Stack.Screen name="(app)" />
-        </Stack.Protected>
-      </Stack>
-    </SafeAreaView>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <SafeAreaView className="flex-1">
+        <StatusBar translucent animated style="auto" />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Protected guard={!session}>
+            <Stack.Screen name="(auth)" />
+          </Stack.Protected>
+          <Stack.Protected guard={!!session}>
+            <Stack.Screen name="(app)" />
+          </Stack.Protected>
+        </Stack>
+      </SafeAreaView>
+    </ThemeProvider>
   )
 }
 
