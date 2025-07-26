936dc6cdde939524dc9e38a77860947c2464c571
Mou <m.mounir.f@gmail.com>
2025-07-26 21:52:26 +0300

    add theme toggle

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,33 @@
+import { Pressable, View } from 'react-native'
+import { setAndroidNavigationBar } from '~/lib/android-navigation-bar'
+import { MoonStarIcon } from '~/lib/icons/MoonStarIcon'
+import { SunIcon } from '~/lib/icons/SunIcon'
+import { useColorScheme } from '~/lib/useColorScheme'
+export function ThemeToggle() {
+  const { isDarkColorScheme, setColorScheme } = useColorScheme()
+
+  function toggleColorScheme() {
+    const newTheme = isDarkColorScheme ? 'light' : 'dark'
+    setColorScheme(newTheme)
+    setAndroidNavigationBar(newTheme)
+  }
+
+  return (
+    <Pressable
+      onPress={toggleColorScheme}
+      className="web:ring-offset-background web:focus-visible:ring-ring active:opacity-70 web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-offset-2"
+    >
+      <View className="flex aspect-square items-start justify-center pt-0.5 web:px-5">
+        {isDarkColorScheme ? (
+          <MoonStarIcon
+            className="text-foreground"
+            size={23}
+            strokeWidth={1.25}
+          />
+        ) : (
+          <SunIcon className="text-foreground" size={24} strokeWidth={1.25} />
+        )}
+      </View>
+    </Pressable>
+  )
+}
